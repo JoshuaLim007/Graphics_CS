a7d195dc5cd7d1e7162d068308a95e9e28788033
Joshua Lim <66184501+JoshuaLim007@users.noreply.github.com>
2024-03-08 18:42:44 -0800

    Added ssgi empty class

---
+++ SSGI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLGraphics
+{
+    public class SSGI : RenderPass
+    {
+        public SSGI(int queueOffset) : base(RenderQueue.AfterTransparents, queueOffset)
+        {
+        }
+
+        public override string Name => "SSGI";
+
+        public override void Execute(in FrameBuffer frameBuffer)
+        {
+
+        }
+
+        protected override void OnDispose()
+        {
+
+        }
+    }
+}
